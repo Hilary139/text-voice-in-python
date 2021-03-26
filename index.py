8ef2e7b025fadc7880bec446db427c9aa7b05df9
flacko0 <juviniercasmir@gmail.com>
2021-03-26 18:26:19 +0100

    made some changes

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import pyttsx3 # importing dependencies
 
 friend = pyttsx3.init() #initializing the pyttsx3 library
 
-friend.say("my name is hilary and i am a good boy") #add text to change to voice 
+friend.say(" I was made by hilary to help him read without stress, i will always serve him as he wishes. i love my creator Hilary. and i will do anything to make him happy") #add text to change to voice 
 
 
 voices = friend.getProperty("voices") # set voice 
